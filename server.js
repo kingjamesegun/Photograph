49dc70a8ca1fadb7d27ce871d88f208ae860c18e
Olamidun <kolapoolamidun@gmail.com>
2021-10-22 02:15:09 +0100

    set up swagger and fixed the bug that makes req.body.password return undefined

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const mongoose = require("mongoose");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
-const userRoute = require("./routes/userRoutes");
-const authRoute = require("./routes/authRoutes");
-const postRoute = require("./routes/postRoutes");
-const bookingRoute = require("./routes/bookingRoutes")
+const userRoute = require("./src/routes/userRoutes");
+const authRoute = require("./src/routes/authRoutes");
+const postRoute = require("./src/routes/postRoutes");
+const bookingRoute = require("./src/routes/bookingRoutes")
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express')
 
 const PORT = 5000;
 
@@ -21,15 +23,32 @@ mongoose.connect('mongodb://localhost:27017/photography', {useNewUrlParser: true
 
 // MIDDLEWARE
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 app.use(morgan("common"));
 
 
+const options = {
+    definition: {
+        swagger: '2.0',
+        info: {
+            title: 'API for Photography website',
+            version: '1.0.0'
+        },
+    },
+    apis: ['src/routes/authRoutes.js']
+}
+
+const swaggerDocs = swaggerJsDoc(options)
+console.log(swaggerDocs)
+
 // ROUTES
+
 app.use("/api/users", userRoute);
 app.use("/api/auths", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/booking", bookingRoute);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 
 // RUNNING THE APP
