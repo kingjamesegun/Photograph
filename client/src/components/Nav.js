159a3fc991260d4485ec443b15d18b1f93756dd2
James <kingjamesegun@gmail.com>
2022-01-05 19:53:07 +0100

    UI

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,61 +1,90 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import '../styles/components/Nav.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/components/Nav.css";
 
 function Nav() {
-    return (
-        <header className="header">
-            <nav className="navbar">
-                <div className="navbar__left">
-                    <Link to="/" className="logo flex justify-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="camera__logo h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"   >
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
-                            </svg>Market
-                        <span style={{color: "orange", fontSize: "24px"}}>.</span>
-                    </Link>
-                    <ul className="nav-menu">
-                        <li className="nav-item">
-                            <Link to="/" className="tracking-wide block uppercase font-bold text-xs active ">Post</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/book" className="tracking-wide block uppercase font-bold text-xs">Book</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/photographers " className="tracking-wide block uppercase font-bold text-xs">Photographers</Link>
-                        </li>
-                    </ul>
-                </div>
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <div className="navbar__left">
+          <Link to="/" className="logo flex justify-center">
+            <div className="text-sans">LensMarket</div>
+          </Link>
+          <ul className="nav-menu">
+            <li className="">
+              <Link
+                to="/"
+                className="tracking-wide block uppercase text-gray-500 font-bold text-xs active font-bold  px-3  rounded hover:bg-blue-500 py-2 border-md hover:text-white text-xs hover:text-white "
+              >
+                Post
+              </Link>
+            </li>
+            <li className="">
+              <Link
+                to="/book"
+                className="tracking-wide block uppercase text-gray-500 font-bold text-xs font-bold  px-3  rounded hover:bg-blue-500 py-2 border-md hover:text-white text-xs hover:text-white"
+              >
+                Book
+              </Link>
+            </li>
+            <li className="">
+              <Link
+                to="/photographers "
+                className="tracking-wide block uppercase text-gray-500 font-bold  px-3  rounded hover:bg-blue-500 py-2 border-md hover:text-white text-xs hover:text-white"
+              >
+                Photographers
+              </Link>
+            </li>
+          </ul>
+        </div>
 
-                <div className="navbar__right">
-                    <ul className="nav-menu">
-                        <li className="nav-item">
-                            <Link to="/notification">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                                </svg>
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/profile">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
-                                </svg>
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+        <div className="navbar__right">
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <Link to="/save">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  />
+                </svg>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/profile">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Link>
+            </li>
+          </ul>
+        </div>
 
-                <div className="hamburger">
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                </div>
-            </nav>
-
-        </header>
-
-    )
+        <div className="hamburger">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Nav
+export default Nav;
--- client/src/components/ProgressStep.js
+++ client/src/components/ProgressStep.js
@@ -1,23 +1,19 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function ProgressLevel() {
-    // const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
 
-
-
-    return (
-        <div>
-          <div className="py-5" >
+  return (
+    <div>
+      <div className="py-5">
         <div className="mx-4 p-4">
           <div className="flex items-center">
             {/* PRERSONAL */}
             <Link to="/progress">
-              <div className="flex items-center text-purple-500 relative">
+              <div className="flex items-center text-white relative">
                 {/* icon */}
-                <div
-                  className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 bg-purple-500 border-purple-500"
-                >
+                <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 bg-blue-500 border-blue-500">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="100%"
@@ -30,29 +26,21 @@ function ProgressLevel() {
                     strokeLinejoin="round"
                     className="feather feather-bookmark stroke-white "
                   >
-                    <path
-                      d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"
-                    ></path>
+                    <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                   </svg>
                 </div>
                 {/* text */}
-                <div
-                  className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-purple-500"
-                >
+                <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-blue-500">
                   Personal
                 </div>
               </div>
             </Link>
-            <div
-              className="flex-auto border-t-2 transition duration-500 ease-in-out border-purple-500"
-            ></div>
+            <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-blue-500"></div>
 
-             {/* des */}
+            {/* des */}
             <Link to="/desc">
               <div className="flex items-center text-white relative">
-                <div
-                  className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 bg-purple-500  border-purple-500"
-                >
+                <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 bg-blue-500  border-blue-500">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="100%"
@@ -71,23 +59,17 @@ function ProgressLevel() {
                     <line x1="23" y1="11" x2="17" y2="11"></line>
                   </svg>
                 </div>
-                <div
-                  className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-purple-500"
-                >
-                    DETAILS
+                <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-blue-500">
+                  SHOT DETAILS
                 </div>
               </div>
             </Link>
-            <div
-              className="flex-auto border-t-2 transition duration-500 ease-in-out border-gray-300"
-            ></div>
+            <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-gray-300"></div>
 
             {/* socials */}
             <Link to="socials">
               <div className="flex items-center text-gray-500 relative">
-                <div
-                  className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-gray-300"
-                >
+                <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-gray-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="100%"
@@ -100,29 +82,21 @@ function ProgressLevel() {
                     strokeLinejoin="round"
                     className="feather feather-mail"
                   >
-                    <path
-                      d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
-                    ></path>
+                    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                     <polyline points="22,6 12,13 2,6"></polyline>
                   </svg>
                 </div>
-                <div
-                  className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-gray-500"
-                >
+                <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-gray-500">
                   SOCIALS
                 </div>
               </div>
             </Link>
 
-            <div
-              className="flex-auto border-t-2 transition duration-500 ease-in-out border-gray-300"
-            ></div>
+            <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-gray-300"></div>
             {/* cCONFIRM */}
             <Link to="/confirm">
               <div className="flex items-center text-gray-500 relative">
-                <div
-                  className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-gray-300"
-                >
+                <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-gray-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="100%"
@@ -140,9 +114,7 @@ function ProgressLevel() {
                     <path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path>
                   </svg>
                 </div>
-                <div
-                  className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-gray-500"
-                >
+                <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-gray-500">
                   Confirm
                 </div>
               </div>
@@ -150,8 +122,8 @@ function ProgressLevel() {
           </div>
         </div>
       </div>
-     
-     {/* LOADER */}
+
+      {/* LOADER */}
       {/* <div className="w-60 h-24 border-2 rounded-md mx-auto mt-20">
         <div className="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
           <div className="w-12 bg-gray-300 h-12 rounded-full ">
@@ -164,9 +136,8 @@ function ProgressLevel() {
           </div>
         </div>
       </div> */}
-
-        </div>
-    )
+    </div>
+  );
 }
 
-export default ProgressLevel
+export default ProgressLevel;
