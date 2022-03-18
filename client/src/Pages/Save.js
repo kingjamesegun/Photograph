4c50ff8bdd3f23b34b18095ff5610330f860311d
James <kingjamesegun@gmail.com>
2022-03-18 21:35:58 +0100

    Navbar and Landing Page

--- client/src/Pages/Save.js
+++ client/src/Pages/Save.js
@@ -1,23 +1,17 @@
-import React from 'react'
-import ProfileCard from '../components/ProfileCard'
-import GalleryPics from '../components/GalleryPicsCard'
-
+import React from "react";
+import GalleryPics from "../components/GalleryPicsCard";
 
 function Save() {
-    return (
-        <div className="save">
-            <div className="body " style={{paddingTop: "2%"}}>
-                <ProfileCard/>
-                <div className="pt-2 font-mono text-lg">
-                    34 saved
-                </div>
-                <div className="row">
-                        <GalleryPics/>
-                </div>
-            </div>
-            
-        </div>
-    )
+	return (
+		<div className='save'>
+			<div className='body ' style={{ paddingTop: "2%" }}>
+				<div className='pt-5 font-mono text-lg'>34 saved</div>
+				<div className='row'>
+					<GalleryPics />
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Save
+export default Save;
